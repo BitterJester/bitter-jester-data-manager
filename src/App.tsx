fef0e3b95d9e2013af7793ddd32733598612c83b
Spencer Kasper <spencer.kasper@gmail.com>
2020-02-26 16:54:50 -0600

    Figured out how to get the .env stuff working.  Needed to prefix all env variables with REACT_APP_

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Title } from './home/Title';
+import aws from 'aws-sdk';
 
 function App() {
+  new aws.S3(
+    {
+      accessKeyId: process.env.REACT_APP_AWS_ACCESS_ID
+    }
+  );
+  console.log(process.env)
   return (
-    <Title titleDisplayText={'Bitter Jester Data Manager'}/>
+    <div>
+      <Title titleDisplayText={'Bitter Jester Data Manager'}/>
+    </div>
   );
 }
 
