3cc8ba33a7de5756dbd403c697af162870b63246
Spencer Kasper <spencer.kasper@gmail.com>
2020-02-26 13:32:41 -0600

    Got tests working and put in the most simple front end view possible.  Also cleaned out all of the boilerplate garbage.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {shallow} from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+    const component = shallow(<App />);
+    it('should load a title', () => {
+        expect(component.find('h1').text()).toEqual('Bitter Jester Data Manager');
+    });
 });
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,1 @@
-/// <reference types="react-scripts" />
+declare module 'enzyme-adapter-react-16'
