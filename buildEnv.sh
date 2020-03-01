18b8814adbb9be3c7f2dc6d540342ebb18e74146
Spencer Kasper <spencer.kasper@gmail.com>
2020-02-29 21:00:58 -0600

    Lets try a different way of setting the environment variables

--- buildEnv.sh
+++ buildEnv.sh
@@ -1,2 +1,3 @@
-${REACT_APP_AWS_ACCESS_ID} >> './.env'
-${REACT_APP_AWS_SECRET_KEY} >> './env'
+echo "REACT_APP_AWS_ACCESS_ID=${REACT_APP_AWS_ACCESS_ID}" >> './.env'
+echo "REACT_APP_AWS_SECRET_KEY=${REACT_APP_AWS_SECRET_KEY}" >> './.env'
+
