cfedf4056b4fc1584d46555d215df845c9118a1e
Spencer Kasper <spencer.kasper@gmail.com>
2020-02-29 21:34:25 -0600

    I think this will work. It has troubles resolving the ~/

--- buildAwsCredentials.sh
+++ buildAwsCredentials.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir ~"/.aws"
+mkdir ~/".aws"
 echo "aws_access_key_id=${REACT_APP_AWS_ACCESS_ID}" > ~/'.aws/credentials'
-echo "aws_secret_access_key=${REACT_APP_AWS_SECRET_KEY}" >> ~'/.aws/credentials'
+echo "aws_secret_access_key=${REACT_APP_AWS_SECRET_KEY}" >> ~/'.aws/credentials'
 
