5b1c1cf66895b89006d7cce6d5ee4334a2c1f55b
Spencer Kasper <spencer.kasper@gmail.com>
2020-02-29 21:39:50 -0600

    Forgot to add default to the config as well

--- buildAwsCredentials.sh
+++ buildAwsCredentials.sh
@@ -5,4 +5,5 @@ echo "[default]" >> ~/'.aws/credntials'
 echo "aws_access_key_id=${REACT_APP_AWS_ACCESS_ID}" >> ~/'.aws/credentials'
 echo "aws_secret_access_key=${REACT_APP_AWS_SECRET_KEY}" >> ~/'.aws/credentials'
 
+echo "[default]" >> ~/'.aws/config'
 echo "region=us-east-1" >> ~/'.aws/config'
