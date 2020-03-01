aa131eef4e3c631ca1909f0314b48e081c36407d
Spencer Kasper <spencer.kasper@gmail.com>
2020-02-29 21:32:02 -0600

    More debug circle

--- buildAwsCredentials.sh
+++ buildAwsCredentials.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 mkdir "~/.aws"
-echo "aws_access_key_id=${REACT_APP_AWS_ACCESS_ID}" > '~/.aws/credentials'
-echo "aws_secret_access_key=${REACT_APP_AWS_SECRET_KEY}" >> '~/.aws/credentials'
+echo $PWD
+echo "aws_access_key_id=${REACT_APP_AWS_ACCESS_ID}" > ~/'.aws/credentials'
+echo "aws_secret_access_key=${REACT_APP_AWS_SECRET_KEY}" >> ~'/.aws/credentials'
 
