702a53e28372891833cf8cdcc14f1838e669d00d
Spencer Kasper <spencer.kasper@gmail.com>
2020-02-29 21:57:20 -0600

    IDK try removing empty folder

--- buildAwsCredentials.sh
+++ buildAwsCredentials.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
 mkdir ~/".aws"
-rm ~/".aws/credentials"
 echo "[default]" > ~/'.aws/credntials'
 echo "aws_access_key_id=${REACT_APP_AWS_ACCESS_ID}" >> ~/'.aws/credentials'
 echo "aws_secret_access_key=${REACT_APP_AWS_SECRET_KEY}" >> ~/'.aws/credentials'
 
-rm ~/".aws/config"
 echo "[default]" > ~/'.aws/config'
 echo "region=us-east-1" >> ~/'.aws/config'
