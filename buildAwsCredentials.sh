4bb6ff431d9e61b3cf22c0c0cf90349cc6711bd5
Spencer Kasper <spencer.kasper@gmail.com>
2020-02-29 21:43:36 -0600

    Create a new file each time this runs

--- buildAwsCredentials.sh
+++ buildAwsCredentials.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 mkdir ~/".aws"
-echo "[default]" >> ~/'.aws/credntials'
+echo "[default]" > ~/'.aws/credntials'
 echo "aws_access_key_id=${REACT_APP_AWS_ACCESS_ID}" >> ~/'.aws/credentials'
 echo "aws_secret_access_key=${REACT_APP_AWS_SECRET_KEY}" >> ~/'.aws/credentials'
 
-echo "[default]" >> ~/'.aws/config'
+echo "[default]" > ~/'.aws/config'
 echo "region=us-east-1" >> ~/'.aws/config'
