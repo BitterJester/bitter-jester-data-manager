3d30727cdf5dc3af1a10edd07cd81aae6b3ce306
Spencer Kasper <spencer.kasper@gmail.com>
2020-02-29 22:00:57 -0600

    I guess i can manage this with a setting in the GUI ugh

--- buildAwsCredentials.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir ~/".aws"
-echo "[default]" > ~/'.aws/credntials'
-echo "aws_access_key_id=${REACT_APP_AWS_ACCESS_ID}" >> ~/'.aws/credentials'
-echo "aws_secret_access_key=${REACT_APP_AWS_SECRET_KEY}" >> ~/'.aws/credentials'
-
-echo "[default]" > ~/'.aws/config'
-echo "region=us-east-1" >> ~/'.aws/config'
