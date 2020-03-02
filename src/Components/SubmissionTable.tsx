0adbe67c4b32053e9da5f0143dd7bf8cbae08333
Spencer Kasper <spencer.kasper@gmail.com>
2020-03-01 22:02:26 -0600

    Remove unused imports

--- src/Components/SubmissionTable.tsx
+++ src/Components/SubmissionTable.tsx
@@ -1,23 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { SubmissionTableRow } from './Table/SubmissionTableRow';
 import { Container } from 'reactstrap';
 import { TableHeader } from './Table/TableHeader';
 import { BitterJesterApplications, BitterJesterApplication } from '../Pages/Submissions/Submissions';
-import { DragDropContext, DropResult, ResponderProvided, Droppable, Draggable } from 'react-beautiful-dnd';
 import { DragAndDropList } from './DragAndDrop/DragAndDropList';
 
 type Props = {
     submissions: BitterJesterApplications;
 }
-
-export type DisplayApplication = {
-    bandName: string;
-    primaryEmailAddress: string;
-    firstChoiceFriday: string;
-    secondChoiceFriday: string;
-}
-
-type DisplayApplications = DisplayApplication[];
 
 export type SubmissionsTableColumnNames = 'Band Name' | 'Primary Email Address' | 'First Choice Friday' | 'Second Choice Friday'
 
