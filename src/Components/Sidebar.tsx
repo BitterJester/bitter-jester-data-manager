5b4b97471bb68d0292ec230a79515ebf0ccaa9ac
Spencer Kasper <spencer.kasper@gmail.com>
2020-03-15 21:50:07 -0500

    No empty hrefs allowed!

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -14,7 +14,7 @@ export const Sidebar = () => {
                 <img src={require('../static/bj15ylogo.png')} alt='BJ15Years' onClick={() => openNav()} />
             </div>
             <div id='mySidenav' className={'sidenav'}>
-                <a href="javascript:void(0)" className="closebtn" onClick={() => closeNav()}>&times;</a>
+                <span className="closebtn" onClick={() => closeNav()}>&times;</span>
                 <a href='/completedSubmissions' >Completed Submissions</a>
                 <a href='/incompleteApplications' >Incomplete Applications</a>
             </div>
