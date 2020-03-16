cd5b94c2dc81c31909655d99317e10c6e27526a1
Spencer Kasper <spencer.kasper@gmail.com>
2020-03-15 21:52:06 -0500

    Update tests oops

--- src/Sidebar.test.tsx
+++ src/Sidebar.test.tsx
@@ -5,11 +5,7 @@ import { Sidebar } from './Components/Sidebar';
 describe('Sidebar', () => {
     const component = shallow(<Sidebar />);
 
-    it('should render a link to close nav bar', () => {
-        expect(component.find('a').at(0).props().href).toEqual('javascript:void(0)');
-    });
-
     it('should render a link to completed submissions', () => {
-        expect(component.find('a').at(1).props().href).toEqual('/completedSubmissions');
+        expect(component.find('a').at(0).props().href).toEqual('/completedSubmissions');
     });
 });
