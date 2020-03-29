33f3ecf9824c3b33d58031667cad3885580c4da5
Spencer Kasper <spencer.kasper@gmail.com>
2020-03-29 16:14:51 -0500

    Set up some basic font global styling

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import './static/default.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
