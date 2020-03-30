5f3dcc0ca5e354972172e55a5ec68a3281e3b611
Spencer Kasper <spencer.kasper@gmail.com>
2020-03-29 20:50:17 -0500

    Still too lazy to write this test lol

--- src/Containers/SubmissionContainer.test.tsx
+++ src/Containers/SubmissionContainer.test.tsx
@@ -1,73 +1,11 @@
 import React from 'react';
+import { SubmissionContainer } from './SubmissionContainer';
+import { shallow } from 'enzyme';
 
 describe('SubmissionTable', () => {
-    const div = <div></div>
-    // const getSubmission = (bandName) => {
-    //     return {
-    //         id: 'id',
-    //         bandName: bandName,
-    //         primaryEmailAddress: 'primaryEmailAddress',
-    //         firstChoiceFridayNight: 'firstChoiceFriday',
-    //         secondChoiceFridayNight: 'secondChoiceFriday',
-    //         isAvailableOnAllFridays: true
-    //     }
-    // }
+    const component = shallow(<div />);
 
-    // const mockSubmissions: BitterJesterApplications = {
-    //     completedApplications: [
-    //         getSubmission('band1'),
-    //         getSubmission('band2')
-    //     ]
-    // };
-    // const component = shallow(<SubmissionContainer submissions={mockSubmissions} />);
-
-    // const container = component.find(Container);
-    // it('should render a container', () => {
-    //     expect(container).toHaveLength(1);
-    // });
-
-    // it('should pass submission object keys as column names to table header', () => {
-    //     const expected: SubmissionsTableColumnNames[] = [
-    //         'Band Name',
-    //         'Primary Email Address',
-    //         'First Choice Friday',
-    //         'Second Choice Friday'
-    //     ];
-    //     expect(component.find(TableHeader).props().tableColumnNamesOrderedFromLeftToRight).toEqual(expected);
-    // });
-
-    // it('should display a title', () => {
-    //     expect(component.find(Title).props().titleDisplayText).toEqual('Completed Submissions');
-    // });
-    
-
-    // describe('Drag and Drop', () => {
-    //     it('should render a drag and drop list', () => {
-    //         expect(component.find(DragAndDropList)).toBeDefined();
-    //     })
-    // });
-
-    // describe('SuggestedSchedule', () => {
-    //     const mockPrunedApplications = [
-    //         {
-    //             bandName: 'band1',
-    //             primaryEmailAddress: 'primaryEmailAddress',
-    //             firstChoiceFridayNight: 'firstChoiceFriday',
-    //             secondChoiceFridayNight: 'secondChoiceFriday'
-    //         },
-    //         {
-    //             bandName: 'band2',
-    //             primaryEmailAddress: 'primaryEmailAddress',
-    //             firstChoiceFridayNight: 'firstChoiceFriday',
-    //             secondChoiceFridayNight: 'secondChoiceFriday'
-    //         }
-    //     ];
-
-    //     it('should pass prunedApplications to suggested schedule component', () => {
-    //         expect(component.find(SuggestedScheduleDragAndDropLists).props().schedule).toEqual(mockPrunedApplications);
-    //     });
-    // })
-    it('should have new tests', () => {
-        expect(true).toEqual(true);
+    it('should have tests', () => {
+        expect(component.find('div')).toBeDefined();
     });
 });
