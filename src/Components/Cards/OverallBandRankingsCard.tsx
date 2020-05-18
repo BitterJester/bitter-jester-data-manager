c71f42966488c32ff6f7d1ce4652932687637b77
Spencer Kasper <spencer.kasper@gmail.com>
2020-05-17 20:07:29 -0500

    * Put all the ranking selection dropdowns in a row

--- src/Components/Cards/OverallBandRankingsCard.tsx
+++ src/Components/Cards/OverallBandRankingsCard.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Card} from 'reactstrap';
+import {Card, Col, Row} from 'reactstrap';
 import {Title} from "../Title";
 import {OriginalSong, OriginalSongs} from "../../Pages/OriginalSongCompetition";
 import RankingDropdown from "../RankingDropdown";
@@ -54,39 +54,47 @@ const OverallBandRankingsCard = (props: Props) => {
     return (
         <Card className={'overall-band-rankings-card'}>
             <Title titleDisplayText={'OVERALL SONG RANKINGS'}/>
-            <div className={'ranking-dropdown-container'}>
-                <h4 className={'ranking-title'}>
-                    1st PLACE
-                </h4>
-                <RankingDropdown originalSongs={originalSongs}
-                                 isOpen={isFirstPlaceOpen}
-                                 toggle={toggleFirst}
-                                 updateSongRankings={updateSongRankings}
-                                 songRankings={songRankings}
-                                 placeToUpdate={'firstPlace'}/>
-            </div>
-            <div className={'ranking-dropdown-container'}>
-                <h4 className={'ranking-title'}>
-                    2nd PLACE
-                </h4>
-                <RankingDropdown originalSongs={originalSongs}
-                                 isOpen={isSecondPlaceOpen}
-                                 toggle={toggleSecond}
-                                 updateSongRankings={updateSongRankings}
-                                 songRankings={songRankings}
-                                 placeToUpdate={'secondPlace'}/>
-            </div>
-            <div className={'ranking-dropdown-container'}>
-                <h4 className={'ranking-title'}>
-                    3rd PLACE
-                </h4>
-                <RankingDropdown originalSongs={originalSongs}
-                                 isOpen={isThirdPlaceOpen}
-                                 toggle={toggleThird}
-                                 updateSongRankings={updateSongRankings}
-                                 songRankings={songRankings}
-                                 placeToUpdate={'thirdPlace'}/>
-            </div>
+            <Row>
+                <Col>
+                    <div className={'ranking-dropdown-container'}>
+                        <h4 className={'ranking-title'}>
+                            1st PLACE
+                        </h4>
+                        <RankingDropdown originalSongs={originalSongs}
+                                         isOpen={isFirstPlaceOpen}
+                                         toggle={toggleFirst}
+                                         updateSongRankings={updateSongRankings}
+                                         songRankings={songRankings}
+                                         placeToUpdate={'firstPlace'}/>
+                    </div>
+                </Col>
+                <Col>
+                    <div className={'ranking-dropdown-container'}>
+                        <h4 className={'ranking-title'}>
+                            2nd PLACE
+                        </h4>
+                        <RankingDropdown originalSongs={originalSongs}
+                                         isOpen={isSecondPlaceOpen}
+                                         toggle={toggleSecond}
+                                         updateSongRankings={updateSongRankings}
+                                         songRankings={songRankings}
+                                         placeToUpdate={'secondPlace'}/>
+                    </div>
+                </Col>
+                <Col>
+                    <div className={'ranking-dropdown-container'}>
+                        <h4 className={'ranking-title'}>
+                            3rd PLACE
+                        </h4>
+                        <RankingDropdown originalSongs={originalSongs}
+                                         isOpen={isThirdPlaceOpen}
+                                         toggle={toggleThird}
+                                         updateSongRankings={updateSongRankings}
+                                         songRankings={songRankings}
+                                         placeToUpdate={'thirdPlace'}/>
+                    </div>
+                </Col>
+            </Row>
         </Card>
     );
 };
