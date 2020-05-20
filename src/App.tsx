751f2d0b27efd20632e25ec21c037e9e06fcd660
Spencer Kasper <spencer.kasper@gmail.com>
2020-05-19 23:01:16 -0500

    * Make every page look neater

--- src/App.tsx
+++ src/App.tsx
@@ -11,43 +11,51 @@ import './App.scss';
 import {Row} from "reactstrap";
 
 function App() {
-  const {isAuthenticated, isInitializing} = useAuth0();
+    const {isAuthenticated, isInitializing} = useAuth0();
 
-  if (isInitializing) {
-    return null;
-  }
+    if (isInitializing) {
+        return null;
+    }
 
-  return (
-      <Router>
-        <Row>
-          <Sidebar/>
-          <div style={{width: '90%'}}>
-            <div id={'content'} className={'content'}>
-              <Switch>
-                <Route exact path='/'>
-                  {isAuthenticated ? 'Please click on the Bitter Jester logo to get access to the rest of the site' : 'Please click on the Bitter Jester logo to get access to login button.'}
-                </Route>
-                <ProtectedRoute
-                    isAuthenticated={isAuthenticated}
-                    path="/completedSubmissions"
-                    component={<Submissions/>}
-                />
-                <ProtectedRoute
-                    isAuthenticated={isAuthenticated}
-                    path='/incompleteApplications'
-                    component={<IncompleteApplications/>}
-                />
-                <ProtectedRoute
-                    isAuthenticated={isAuthenticated}
-                    path='/originalSong'
-                    component={<OriginalSongCompetition/>}
-                />
-              </Switch>
-            </div>
-          </div>
-        </Row>
-      </Router>
-  )
+    return (
+        <Router>
+            <Row>
+                <Sidebar/>
+                <div style={{width: '90%'}}>
+                    <div id={'content'} className={'content'}>
+                        <Switch>
+                            <Route exact path='/'>
+                                <div style={{background: 'rgb(232, 231, 228)'}}
+                                     className={'original-song-competition-container'}>
+                                    <div style={{textAlign: "center", padding: '16px'}}
+                                         className={'original-song-container'}>
+                                        {isAuthenticated ?
+                                            'Please click on the Bitter Jester logo to get access to the rest of the site' :
+                                            'Please click on the Bitter Jester logo to get access to login button.'}
+                                    </div>
+                                </div>
+                            </Route>
+                            <ProtectedRoute
+                                isAuthenticated={isAuthenticated}
+                                path="/completedSubmissions"
+                                component={<Submissions/>}
+                            />
+                            <ProtectedRoute
+                                isAuthenticated={isAuthenticated}
+                                path='/incompleteApplications'
+                                component={<IncompleteApplications/>}
+                            />
+                            <ProtectedRoute
+                                isAuthenticated={isAuthenticated}
+                                path='/originalSong'
+                                component={<OriginalSongCompetition/>}
+                            />
+                        </Switch>
+                    </div>
+                </div>
+            </Row>
+        </Router>
+    )
 }
 
 export default App;
