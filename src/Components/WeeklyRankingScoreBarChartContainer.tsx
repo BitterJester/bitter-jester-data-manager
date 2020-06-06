ce2e57dca297ae8949efdeecb3524118adb69411
Spencer Kasper <spencer.kasper@gmail.com>
2020-06-05 19:46:40 -0500

    * Sort from highest to lowest

--- src/Components/WeeklyRankingScoreBarChartContainer.tsx
+++ src/Components/WeeklyRankingScoreBarChartContainer.tsx
@@ -9,17 +9,24 @@ type SongRankingTotal = {
 }
 
 export type SongRankingTotals = {
+    allSongsAreSubmitted: boolean;
+    totalFinalRankings: number;
     totalScores: SongRankingTotal[];
 };
 
 const WeeklyRankingScoreBarChartContainer = () => {
-    const initialSongRankingTotals: SongRankingTotals = {totalScores: []};
+    const initialSongRankingTotals: SongRankingTotals = {
+        totalScores: [],
+        totalFinalRankings: 0,
+        allSongsAreSubmitted: false
+    };
     const [songRankingTotals, setSongRankingTotals] = useState(initialSongRankingTotals);
 
     useEffect(() => {
         const fetch = async () => {
-            const fetchedSongRankingTotals = await new S3Client().getObject('song-ranking-totals.json');
-            setSongRankingTotals(fetchedSongRankingTotals as SongRankingTotals);
+            const fetchedSongRankingTotals = await new S3Client().getObject('song-ranking-totals.json') as SongRankingTotals;
+            fetchedSongRankingTotals.totalScores = fetchedSongRankingTotals.totalScores.sort((a, b) => a.totalPoints > b.totalPoints ? -1 : 1);
+            setSongRankingTotals(fetchedSongRankingTotals);
         };
 
         fetch();
@@ -27,7 +34,19 @@ const WeeklyRankingScoreBarChartContainer = () => {
 
     return (
         <div className={'original-song-competition-container'}>
-            <WeeklyRankingScoreBarChart songRankingTotals={songRankingTotals}/>
+            <div style={{textAlign: "center"}}>
+                <p>
+                    {`Total Final Rankings: ${songRankingTotals.totalFinalRankings}`}
+                </p>
+                {songRankingTotals.allSongsAreSubmitted &&
+                <p>
+                    {'All rankings have been submitted for this week.'}
+                </p>
+                }
+            </div>
+            <div>
+                <WeeklyRankingScoreBarChart songRankingTotals={songRankingTotals}/>
+            </div>
         </div>
     );
 };
