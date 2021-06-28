2e13b64c27e98a8acd316f98e2d214a4cd0f2f93
spencerkasper <spener.kasper@gmail.com>
2021-06-28 00:12:44 -0500

    * Fix the count

--- src/Components/SuggestedSchedule/ScheduleToolbar.tsx
+++ src/Components/SuggestedSchedule/ScheduleToolbar.tsx
@@ -1,12 +1,10 @@
 import React, { useState, Fragment } from 'react';
-import { Row, Alert } from 'reactstrap';
+import { Alert } from 'reactstrap';
 import ScheduleDropdown from './ScheduleDropdown';
 import { Schedule, SUGGESTED_FRIDAY_NIGHT_SCHEDULE } from '../../containers/ScheduleContainer';
 import SaveScheduleButton from './SaveScheduleButton';
 import TotalCount from '../TotalCount';
 import CompetitionBandsMultiSelectCheckboxDropdown from "./CompetitionBandsMultiSelectCheckboxDropdown";
-import {DataManagerReduxStore} from "../../redux/data-manager-redux-store";
-import {useSelector} from "react-redux";
 
 const USER_FRIDAY_NIGHT_SCHEDULE = 'user-friday-night-schedule.json';
 
@@ -17,7 +15,6 @@ type Props = {
 
 const ScheduleToolbar = (props: Props) => {
     const { schedule, updateSchedule } = props;
-    const removedBands = useSelector((state: DataManagerReduxStore) => state.selectedCompetition.removedBands);
     const [isSaveAlertOpen, setIsSaveAlertOpen] = useState(false);
 
     const onAlert = () => {
@@ -25,7 +22,7 @@ const ScheduleToolbar = (props: Props) => {
     }
 
     const getTotalBands = (): number => {
-        return schedule.nights.reduce((count, night) => count + night.bands.length, 0) - removedBands.length;
+        return schedule.nights.reduce((count, night) => count + night.bands.length, 0);
     }
     
     return (
