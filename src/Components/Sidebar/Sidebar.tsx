3848f9805f44a7c9921e59165868dedfdfc4e1f7
spencerkasper <spener.kasper@gmail.com>
2021-07-05 22:39:55 -0500

    * Add more to the sidebar

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -7,11 +7,14 @@ interface Props extends RouteComponentProps {
 
 }
 
+const HOME_PATH = '/';
+const COMPLETED_SUBMISSIONS_PATH = '/completedSubmissions';
+const UPLOADED_FILES_PATH = '/uploadedFiles';
+const INCOMPLETE_APPLICATIONS_PATH = '/incompleteApplications';
 const Sidebar = (props: Props) => {
     const domain = window.location.href.split('/')[2];
     const protocol = window.location.protocol;
     const {isAuthenticated, loginWithRedirect, logout} = useAuth0();
-    const [selectedCompetitions, updateSelectedCompetitions] = useState([]);
 
     const getURI = (path: string) => {
         const fullDomain = `${protocol}//${domain}`;
@@ -19,7 +22,8 @@ const Sidebar = (props: Props) => {
     }
 
     const redirect = (path: string) => {
-        props.history.push(path);
+        const queryParams = window.location.search;
+        props.history.push(`${path}${queryParams}`);
     }
 
     const getAuthenticationButtonText = () => {
@@ -36,13 +40,17 @@ const Sidebar = (props: Props) => {
     const logoUrl = window.location.pathname === '/originalSong' ?
         require('../../static/images/bj-14-5-logo.png') :
         require('../../static/images/bj15ylogo.png');
+    const notOnHomePage = window.location.pathname !== HOME_PATH;
     return (
         <Col className={'sidebar-container'}>
             <div className={'sidebar-logo'}>
                 <img height={200} src={logoUrl} alt='BJ15Years'/>
             </div>
             <div className={'sidebar'}>
-                <button className={'sidebar-button'} onClick={() => redirect('/')}>Home</button>
+                {notOnHomePage && <button className={'sidebar-button'} onClick={() => redirect(HOME_PATH)}>Home</button>}
+                {notOnHomePage && window.location.pathname  !== COMPLETED_SUBMISSIONS_PATH && <button className={'sidebar-button'} onClick={() => redirect(COMPLETED_SUBMISSIONS_PATH)}>Completed Submissions</button>}
+                {notOnHomePage && window.location.pathname !== UPLOADED_FILES_PATH && <button className={'sidebar-button'} onClick={() => redirect(UPLOADED_FILES_PATH)}>Uploaded Files</button>}
+                {notOnHomePage && window.location.pathname !== INCOMPLETE_APPLICATIONS_PATH && <button className={'sidebar-button'} onClick={() => redirect(INCOMPLETE_APPLICATIONS_PATH)}>Incomplete Applications</button>}
                 <button className={'sidebar-button'} onClick={authenticate}>{getAuthenticationButtonText()}</button>
             </div>
         </Col>
