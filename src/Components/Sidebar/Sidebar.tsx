81030220f7040933f115d6000494ed86287b5ca0
spencerkasper <spener.kasper@gmail.com>
2022-03-13 21:07:43 -0500

    * Also get rid of dynamic loading of sidebar links.

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -20,17 +20,16 @@ const Sidebar = (props: Props) => {
     const logoUrl = window.location.pathname === '/originalSong' ?
         require('../../static/images/bj-14-5-logo.png') :
         require('../../static/images/bj15ylogo.png');
-    const notOnHomePage = window.location.pathname !== HOME_PATH;
     return (
         <Col className={'sidebar-container'}>
             <div className={'sidebar-logo'}>
                 <img height={200} src={logoUrl} alt='BJ15Years'/>
             </div>
             <div className={'sidebar'}>
-                {notOnHomePage && <button className={'sidebar-button'} onClick={() => redirect(HOME_PATH)}>Home</button>}
-                {notOnHomePage && window.location.pathname  !== COMPLETED_SUBMISSIONS_PATH && <button className={'sidebar-button'} onClick={() => redirect(COMPLETED_SUBMISSIONS_PATH)}>Completed Submissions</button>}
-                {notOnHomePage && window.location.pathname !== UPLOADED_FILES_PATH && <button className={'sidebar-button'} onClick={() => redirect(UPLOADED_FILES_PATH)}>Uploaded Files</button>}
-                {notOnHomePage && window.location.pathname !== INCOMPLETE_APPLICATIONS_PATH && <button className={'sidebar-button'} onClick={() => redirect(INCOMPLETE_APPLICATIONS_PATH)}>Incomplete Applications</button>}
+                <button className={'sidebar-button'} onClick={() => redirect(HOME_PATH)}>Home</button>
+                <button className={'sidebar-button'} onClick={() => redirect(COMPLETED_SUBMISSIONS_PATH)}>Completed Submissions</button>
+                <button className={'sidebar-button'} onClick={() => redirect(UPLOADED_FILES_PATH)}>Uploaded Files</button>
+                <button className={'sidebar-button'} onClick={() => redirect(INCOMPLETE_APPLICATIONS_PATH)}>Incomplete Applications</button>
                 <AmplifySignOut />
             </div>
         </Col>
