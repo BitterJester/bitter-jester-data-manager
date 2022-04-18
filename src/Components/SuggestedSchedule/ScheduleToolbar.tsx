76e71bab2f2ad20a75a0b89235f6b0850f6722c3
spencerkasper <spener.kasper@gmail.com>
2022-04-18 18:49:07 -0500

    * Use toast instead of alert.

--- src/Components/SuggestedSchedule/ScheduleToolbar.tsx
+++ src/Components/SuggestedSchedule/ScheduleToolbar.tsx
@@ -1,10 +1,11 @@
-import React, { useState, Fragment } from 'react';
-import { Alert } from 'reactstrap';
+import React, {useState, Fragment} from 'react';
+import {Alert} from 'reactstrap';
 import ScheduleDropdown from './ScheduleDropdown';
 import {LAST_SAVE_VERSION, Schedule, SUGGESTED_VERSION} from '../../containers/ScheduleContainer';
 import SaveScheduleButton from './SaveScheduleButton';
 import TotalCount from '../TotalCount';
 import CompetitionBandsMultiSelectCheckboxDropdown from "./CompetitionBandsMultiSelectCheckboxDropdown";
+import {toast} from "react-toastify";
 
 type Props = {
     schedule: Schedule;
@@ -12,31 +13,34 @@ type Props = {
 }
 
 const ScheduleToolbar = (props: Props) => {
-    const { schedule, updateSchedule } = props;
-    const [isSaveAlertOpen, setIsSaveAlertOpen] = useState(false);
+    const {schedule, updateSchedule} = props;
 
     const onAlert = () => {
-        setIsSaveAlertOpen(false);
-        setIsSaveAlertOpen(true);
+        toast.success('Successfully updated the saved schedule!');
     }
 
     const getTotalBands = (): number => {
         return schedule.nights.reduce((count, night) => count + night.bands.length, 0);
     }
-    
+
     return (
         <Fragment>
-            <Alert isOpen={isSaveAlertOpen} toggle={onAlert} style={{ textAlign: 'center' }}>The schedule has been updated!</Alert>
-            <div style={{display: 'flex', width: '100%', justifyContent: 'space-between', alignItems: 'baseline', flexWrap: 'wrap'}}>
+            <div style={{
+                display: 'flex',
+                width: '100%',
+                justifyContent: 'space-between',
+                alignItems: 'baseline',
+                flexWrap: 'wrap'
+            }}>
                 <div style={{display: 'flex', width: '50%'}}>
                     <ScheduleDropdown
                         dropdownItemOnClick={() => updateSchedule(SUGGESTED_VERSION)}
                         dropdownItemOnClick2={() => updateSchedule(LAST_SAVE_VERSION)}
                     />
-                    <SaveScheduleButton schedule={schedule} onAlert={onAlert} />
+                    <SaveScheduleButton schedule={schedule} onAlert={onAlert}/>
                 </div>
-                <div style={{ width: '50%', textAlign: 'right', paddingRight: '32px'}}>
-                    <CompetitionBandsMultiSelectCheckboxDropdown />
+                <div style={{width: '50%', textAlign: 'right', paddingRight: '32px'}}>
+                    <CompetitionBandsMultiSelectCheckboxDropdown/>
                     <TotalCount count={getTotalBands()}/>
                 </div>
             </div>
