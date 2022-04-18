edde6b9488378ae5b01fa4bd36dc43651a63f0ca
spencerkasper <spener.kasper@gmail.com>
2022-04-18 15:16:57 -0500

    * Fix the colors

--- src/Components/SuggestedSchedule/BackgroundColor.tsx
+++ src/Components/SuggestedSchedule/BackgroundColor.tsx
@@ -13,20 +13,18 @@ export class BackgroundColor {
         const nightMap = getNightMap(competitionId);
         const nightParts = nightMap[String(this.night)];
         const dayOfTheMonth = nightParts.dayOfTheMonth;
-        const didGetFirstChoice = this.application.firstChoiceFridayNight.includes(dayOfTheMonth) ||
+        const didGetFirstChoice = this.application.firstChoiceFridayNight.includes(` ${dayOfTheMonth},`) ||
             this.application.firstChoiceFridayNight === 'Available Every Friday';
         if(didGetFirstChoice) {
             return 'darkgreen';
         }
-
-        const didGetSecondChoice = this.application.secondChoiceFridayNight.includes(dayOfTheMonth);
+        const didGetSecondChoice = this.application.secondChoiceFridayNight.includes(` ${dayOfTheMonth},`);
 
         if(didGetSecondChoice){
             return 'rgb(227, 194, 27)';
         }
-
-        const isUnavailable = this.application.unavailableFridayNights ? 
-            Boolean(this.application.unavailableFridayNights.map(night => night.includes(dayOfTheMonth)).filter(item => item === true).length) :
+        const isUnavailable = this.application.unavailableFridayNights ?
+            Boolean(this.application.unavailableFridayNights.map(night => night.includes(` ${dayOfTheMonth},`)).filter(item => item === true).length) :
             false;
 
         if(isUnavailable){
